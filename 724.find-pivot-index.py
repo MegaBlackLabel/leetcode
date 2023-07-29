6e9b859427271409aa8a2949c4de414e34d07c77
底なし沼の魔女 <megablacklabel@gmail.com>
2023-07-29 04:19:53 +0000

    answered

---
+++ 724.find-pivot-index.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=724 lang=python3
+#
+# [724] Find Pivot Index
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        summ = sum(nums)
+        prefix = 0
+
+        for i, num in enumerate(nums):
+            if prefix == summ - prefix - num:
+                return i
+            prefix += num
+
+        return -1
+# @lc code=end
+
