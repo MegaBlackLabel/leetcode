985e8528bede305f0ef1de08f6a3c4373201d48d
底なし沼の魔女 <megablacklabel@gmail.com>
2023-08-06 03:34:34 +0000

    answered

---
+++ 766.toeplitz-matrix.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=766 lang=python3
+#
+# [766] Toeplitz Matrix
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        for i in range(len(matrix) - 1):
+            for j in range(len(matrix[0]) - 1):
+                if matrix[i][j] != matrix[i + 1][j + 1]:
+                    return False
+
+        return True
+# @lc code=end
+
