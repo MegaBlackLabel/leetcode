4f4da02745288bdc2e2b9dcd1c2ba413ba16b5e7
底なし沼の魔女 <megablacklabel@gmail.com>
2023-08-14 01:31:26 +0000

    answered

---
+++ 819.most-common-word.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=819 lang=python3
+#
+# [819] Most Common Word
+#
+
+# @lc code=start
+import collections
+import re
+from typing import List
+
+
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        banned = set(banned)
+        words = re.findall(r'\w+', paragraph.lower())
+        return collections.Counter(word for word in words if word not in banned).most_common(1)[0][0]
+# @lc code=end
+
