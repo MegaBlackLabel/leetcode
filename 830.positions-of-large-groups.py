7824848a309b87f3948885725c3f7f882a1848b3
底なし沼の魔女 <megablacklabel@gmail.com>
2023-08-17 03:23:14 +0000

    answered

---
+++ 830.positions-of-large-groups.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=830 lang=python3
+#
+# [830] Positions of Large Groups
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def largeGroupPositions(self, s: str) -> List[List[int]]:
+        n = len(s)
+        ans = []
+        i = 0
+        j = 0
+
+        while i < n:
+            while j < n and s[j] == s[i]:
+                j += 1
+            if j - i >= 3:
+                ans.append([i, j - 1])
+            i = j
+
+        return ans
+# @lc code=end
+
