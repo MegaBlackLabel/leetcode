40a4c17e17b99ba218cb65c66da78a71d64a4579
底なし沼の魔女 <megablacklabel@gmail.com>
2023-09-06 03:04:05 +0000

    answered

---
+++ 908.smallest-range-i.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=908 lang=python3
+#
+# [908] Smallest Range I
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def smallestRangeI(self, nums: List[int], k: int) -> int:
+        return max(0, max(nums) - min(nums) - 2 * k)
+# @lc code=end
+
