e32bc0c6d5baff7daf1b65f77bc984ae8fb890f0
底なし沼の魔女 <megablacklabel@gmail.com>
2023-09-11 03:12:30 +0000

    answered

---
+++ 929.unique-email-addresses.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=929 lang=python3
+#
+# [929] Unique Email Addresses
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        seen = set()
+
+        for email in emails:
+            local, domain = email.split('@')
+            local = local.split('+')[0].replace('.', '')
+            seen.add(local + '@' + domain)
+
+        return len(seen)
+# @lc code=end
+
