9a11df867a17b7a0a2bb01717e8f7ae62a17be1c
底なし沼の魔女 <megablacklabel@gmail.com>
2023-11-14 03:20:33 +0000

    answered

---
+++ 1331.rank-transform-of-an-array.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=1331 lang=python3
+#
+# [1331] Rank Transform of an Array
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        rank = {}
+
+        for a in sorted(arr):
+            if a not in rank:
+                rank[a] = len(rank) + 1
+
+        return map(rank.get, arr)
+# @lc code=end
+
