22a33ff6b2f560d4ddc3949ec3b2d2425d37ff22
底なし沼の魔女 <megablacklabel@gmail.com>
2023-12-26 03:14:18 +0000

    answered

---
+++ 1523.count-odd-numbers-in-an-interval-range.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1523 lang=python3
+#
+# [1523] Count Odd Numbers in an Interval Range
+#
+
+# @lc code=start
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        return ((high + 1) >> 1) - (low >> 1)
+# @lc code=end
+
