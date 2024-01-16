3a46d8706d5b884f7029a10a46b1335e75f02dac
底なし沼の魔女 <megablacklabel@gmail.com>
2024-01-16 03:11:08 +0000

    answered

---
+++ 1636.sort-array-by-increasing-frequency.py
@@ -0,0 +1,39 @@
+#
+# @lc app=leetcode id=1636 lang=python3
+#
+# [1636] Sort Array by Increasing Frequency
+#
+
+# @lc code=start
+
+import collections
+import heapq
+from typing import List
+
+
+class T:
+  def __init__(self, num: int, freq: int):
+    self.num = num
+    self.freq = freq
+
+  def __lt__(self, other):
+    if self.freq == other.freq:
+      return self.num > other.num
+    return self.freq < other.freq
+
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        ans = []
+        heap = []
+
+        for num, freq in collections.Counter(nums).items():
+            heapq.heappush(heap, T(num, freq))
+
+        while len(heap) > 0:
+            num = heap[0].num
+            freq = heapq.heappop(heap).freq
+            ans.extend([num] * freq)
+
+        return ans
+# @lc code=end
+
