5a95a91e7bc92e3dd9148cc618ca94aaa8ae3457
底なし沼の魔女 <megablacklabel@gmail.com>
2024-01-19 03:20:37 +0000

    answered

---
+++ 1652.defuse-the-bomb.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=1652 lang=python3
+#
+# [1652] Defuse the Bomb
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        n = len(code)
+        ans = [0] * n
+        if k == 0:
+            return ans
+
+        summ = 0
+        start = 1 if k > 0 else n + k  # the start of the next k numbers
+        end = k if k > 0 else n - 1  # the end of the next k numbers
+
+        for i in range(start, end + 1):
+            summ += code[i]
+
+        for i in range(n):
+            ans[i] = summ
+            summ -= code[start % n]
+            start += 1
+            end += 1
+            summ += code[end % n]
+
+        return ans
+# @lc code=end
+
