e6e8c250face7e329c7238e27b02b4d806264264
底なし沼の魔女 <megablacklabel@gmail.com>
2024-02-11 04:31:25 +0000

    answered

---
+++ 1768.merge-strings-alternately.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=1768 lang=python3
+#
+# [1768] Merge Strings Alternately
+#
+
+# @lc code=start
+from itertools import zip_longest
+
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        return ''.join(a + b for a, b in zip_longest(word1, word2, fillvalue=''))
+    
+    
+# @lc code=end
+
