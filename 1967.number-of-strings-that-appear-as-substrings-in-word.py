b5e98d48f594a15a812d554aaf36a7f84b3286cf
底なし沼の魔女 <megablacklabel@gmail.com>
2024-03-20 03:57:46 +0000

    1967番目の単語にサブストリングとして表示される文字列の数を追加

---
+++ 1967.number-of-strings-that-appear-as-substrings-in-word.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=1967 lang=python3
+#
+# [1967] Number of Strings That Appear as Substrings in Word
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def numOfStrings(self, patterns: List[str], word: str) -> int:
+        return sum(pattern in word for pattern in patterns)
+# @lc code=end
+
