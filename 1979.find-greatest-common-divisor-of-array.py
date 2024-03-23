65e9c6778d8238909df65925aa30e2357aec5882
底なし沼の魔女 <megablacklabel@gmail.com>
2024-03-23 03:25:01 +0000

    1979番目の配列の最大公約数を見つける

---
+++ 1979.find-greatest-common-divisor-of-array.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=1979 lang=python3
+#
+# [1979] Find Greatest Common Divisor of Array
+#
+
+# @lc code=start
+import math
+from typing import List
+
+
+class Solution:
+    def findGCD(self, nums: List[int]) -> int:
+        return math.gcd(min(nums), max(nums))
+    
+# @lc code=end
+
