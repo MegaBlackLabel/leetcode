686f32a7c190e52086bd7dd3afb63e5f12edf64b
底なし沼の魔女 <megablacklabel@gmail.com>
2024-04-18 03:18:46 +0000

    2114. 文章中で最も多くの単語が見つかるコードを追加

---
+++ 2114.maximum-number-of-words-found-in-sentences.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=2114 lang=python3
+#
+# [2114] Maximum Number of Words Found in Sentences
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def mostWordsFound(self, sentences: List[str]) -> int:
+        return max(s.count(' ') for s in sentences) + 1
+    
+# @lc code=end
+
