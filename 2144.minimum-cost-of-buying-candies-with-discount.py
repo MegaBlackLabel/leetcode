d5e6efd76e40280d41c22f525a82e38cbc8970fc
底なし沼の魔女 <megablacklabel@gmail.com>
2024-04-24 03:11:23 +0000

    2144. キャンディを割引価格で購入するための最小コストのコードを追加

---
+++ 2144.minimum-cost-of-buying-candies-with-discount.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=2144 lang=python3
+#
+# [2144] Minimum Cost of Buying Candies With Discount
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def minimumCost(self, cost: List[int]) -> int:
+        return sum(cost) - sum(sorted(cost)[-3::-3])
+    
+# @lc code=end
+
