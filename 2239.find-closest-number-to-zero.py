0165f387af2ed60a65319a98c52664d87660632a
底なし沼の魔女 <megablacklabel@gmail.com>
2024-05-14 02:54:11 +0000

    feat: Add solution for finding the closest number to zero

---
+++ 2239.find-closest-number-to-zero.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=2239 lang=python3
+#
+# [2239] Find Closest Number to Zero
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def findClosestNumber(self, nums: List[int]) -> int:
+        nums.sort(key=lambda x: (abs(x), -x))
+        
+        return nums[0]
+# @lc code=end
+
