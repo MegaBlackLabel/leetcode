10bf1822f80a0c2112b327af32b5be1d94030245
底なし沼の魔女 <megablacklabel@gmail.com>
2024-05-17 03:30:38 +0000

    feat: Add solution for counting prefixes of a given string

---
+++ 2255.count-prefixes-of-a-given-string.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=2255 lang=python3
+#
+# [2255] Count Prefixes of a Given String
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def countPrefixes(self, words: List[str], s: str) -> int:
+        return sum(map(s.startswith, words))
+# @lc code=end
+
