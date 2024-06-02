22d8884ed06864a69915bf3c8a62e0cc31c5672b
底なし沼の魔女 <megablacklabel@gmail.com>
2024-06-02 02:57:20 +0000

    feat: Add solution for implementing the Minimum Amount of Time to Fill Cups problem

---
+++ 2335.minimum-amount-of-time-to-fill-cups.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=2335 lang=python3
+#
+# [2335] Minimum Amount of Time to Fill Cups
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def fillCups(self, amount: List[int]) -> int:
+        return max(max(amount), (sum(amount) + 1) // 2)
+# @lc code=end
+
