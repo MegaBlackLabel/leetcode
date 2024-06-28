d670c3e9a86f9b14f1cae84b005e82ab961d4c82
底なし沼の魔女 <megablacklabel@gmail.com>
2024-06-28 03:02:46 +0000

    feat: Add solution for implementing the Convert the Temperature problem

---
+++ 2469.convert-the-temperature.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=2469 lang=python3
+#
+# [2469] Convert the Temperature
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        return [celsius + 273.15, celsius * 1.8 + 32]
+# @lc code=end
+
