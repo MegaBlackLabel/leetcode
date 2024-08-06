acedb5499db0bec563092447cd3fd3486b27c4db
底なし沼の魔女 <megablacklabel@gmail.com>
2024-08-06 03:05:45 +0000

    feat: Add solution for implementing the Find the Distinct Difference Array problem

---
+++ 2670.find-the-distinct-difference-array.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode id=2670 lang=python3
+#
+# [2670] Find the Distinct Difference Array
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def distinctDifferenceArray(self, nums: List[int]) -> List[int]:
+        kMax = 50
+        ans = []
+        prefixCount = [0] * (kMax + 1)
+        suffixCount = [0] * (kMax + 1)
+        distinctPrefix = 0
+        distinctSuffix = 0
+
+        for num in nums:
+            if suffixCount[num] == 0:
+                distinctSuffix += 1
+            suffixCount[num] += 1
+
+        for num in nums:
+            if prefixCount[num] == 0:
+                distinctPrefix += 1
+            prefixCount[num] += 1
+            if suffixCount[num] == 1:
+                distinctSuffix -= 1
+            suffixCount[num] -= 1
+            ans.append(distinctPrefix - distinctSuffix)
+
+        return ans
+# @lc code=end
+
