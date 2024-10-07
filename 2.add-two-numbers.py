03c424336c9cc82e5b51626d978d1f1337f90d9b
底なし沼の魔女 <megablacklabel@gmail.com>
2024-10-07 02:26:43 +0000

    feat: Implement solution for the Add Two Numbers problem

---
+++ 2.add-two-numbers.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode id=2 lang=python3
+#
+# [2] Add Two Numbers
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        curr = dummy
+        carry = 0
+
+        while carry or l1 or l2:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            curr.next = ListNode(carry % 10)
+            carry //= 10
+            curr = curr.next
+
+        return dummy.next
+# @lc code=end
+
