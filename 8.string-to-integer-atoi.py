9281248c036f4d09a10101fc73b85d04448051f7
底なし沼の魔女 <megablacklabel@gmail.com>
2024-10-13 05:37:30 +0000

    feat: Implement solution for the String to Integer (atoi) problem

---
+++ 8.string-to-integer-atoi.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=8 lang=python3
+#
+# [8] String to Integer (atoi)
+#
+
+# @lc code=start
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        s = s.strip()
+        if not s:
+            return 0
+
+        sign = -1 if s[0] == '-' else 1
+    
+        if s[0] in {'-', '+'}:
+            s = s[1:]
+
+        num = 0
+
+        for c in s:
+            if not c.isdigit():
+                break
+            num = num * 10 + int(c)
+            if sign * num <= -2**31:
+                return -2**31
+            if sign * num >= 2**31 - 1:
+                return 2**31 - 1
+
+        return sign * num
+# @lc code=end
+
