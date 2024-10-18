77308f975dfc138f9acc24bc587cdc640aa0c36f
底なし沼の魔女 <megablacklabel@gmail.com>
2024-10-18 03:09:25 +0000

    feat: Implement solution for the 3Sum Closest problem

---
+++ 16.3-sum-closest.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode id=16 lang=python3
+#
+# [16] 3Sum Closest
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        ans = nums[0] + nums[1] + nums[2]
+
+        nums.sort()
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            l = i + 1
+            r = len(nums) - 1
+            while l < r:
+                summ = nums[i] + nums[l] + nums[r]
+                if summ == target:
+                    return summ
+                if abs(summ - target) < abs(ans - target):
+                    ans = summ
+                if summ < target:
+                    l += 1
+                else:
+                    r -= 1
+
+        return ans
+# @lc code=end
+
