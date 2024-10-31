990f3fe6b43e3994789f35b0ceb16bb73d7e3bca
底なし沼の魔女 <megablacklabel@gmail.com>
2024-10-31 03:22:13 +0000

    feat: Implement solution for the Count and Say problem

---
+++ 38.count-and-say.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=38 lang=python3
+#
+# [38] Count and Say
+#
+
+# @lc code=start
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        ans = '1'
+
+        for _ in range(n - 1):
+            nxt = ''
+            i = 0
+            while i < len(ans):
+                count = 1
+                while i + 1 < len(ans) and ans[i] == ans[i + 1]:
+                    count += 1
+                    i += 1
+                nxt += str(count) + ans[i]
+                i += 1
+            ans = nxt
+
+        return ans
+# @lc code=end
+
