bf7fd12bd50ba11247e23d0bb182b1752c59fd58
底なし沼の魔女 <megablacklabel@gmail.com>
2024-11-14 03:23:51 +0000

    feat: Implement solution for the Merge Intervals problem

---
+++ 56.merge-intervals.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=56 lang=python3
+#
+# [56] Merge Intervals
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        ans = []
+
+        for interval in sorted(intervals):
+            if not ans or ans[-1][1] < interval[0]:
+                ans.append(interval)
+            else:
+                ans[-1][1] = max(ans[-1][1], interval[1])
+
+        return ans
+# @lc code=end
+
