c91aae48c3405f216e1af0265e38727b0077f7b8
底なし沼の魔女 <megablacklabel@gmail.com>
2024-11-26 05:33:11 +0000

    feat: Implement solution for the Sort Colors problem

---
+++ 75.sort-colors.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=75 lang=python3
+#
+# [75] Sort Colors
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l = 0 
+        r = len(nums) - 1
+
+        i = 0
+        while i <= r:
+            if nums[i] == 0:
+                nums[i], nums[l] = nums[l], nums[i]
+                i += 1
+                l += 1
+            elif nums[i] == 1:
+                i += 1
+            else:
+                nums[i], nums[r] = nums[r], nums[i]
+                r -= 1
+# @lc code=end
+
