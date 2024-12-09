f508306f944ccab1451a8ec0f78faa176461b4d7
底なし沼の魔女 <megablacklabel@gmail.com>
2024-12-09 03:25:55 +0000

    feat: Implement solution for the Unique Binary Search Trees II problem

---
+++ 95.unique-binary-search-trees-ii.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=95 lang=python3
+#
+# [95] Unique Binary Search Trees II
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+from typing import List, Optional
+
+
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
+        def dfs(i: int, j: int) -> List[Optional[TreeNode]]:
+            if i > j:
+                return [None]
+            ans = []
+            for v in range(i, j + 1):
+                left = dfs(i, v - 1)
+                right = dfs(v + 1, j)
+                for l in left:
+                    for r in right:
+                        ans.append(TreeNode(v, l, r))
+            return ans
+
+        return dfs(1, n)
+# @lc code=end
+
