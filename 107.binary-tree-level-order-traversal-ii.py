571278cbd91732aaddb000a59321f53074ce9631
底なし沼の魔女 <megablacklabel@gmail.com>
2024-12-19 03:36:41 +0000

    feat: Implement solution for the Binary Tree Level Order Traversal II problem

---
+++ 107.binary-tree-level-order-traversal-ii.py
@@ -0,0 +1,39 @@
+#
+# @lc app=leetcode id=107 lang=python3
+#
+# [107] Binary Tree Level Order Traversal II
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+import collections
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+
+        ans = []
+        q = collections.deque([root])
+
+        while q:
+            currLevel = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                currLevel.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            ans.append(currLevel)
+
+        return ans[::-1]
+# @lc code=end
+
