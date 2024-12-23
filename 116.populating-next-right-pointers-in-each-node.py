27f50bf0110cb834d4ee2803f51731d48bf3b98a
底なし沼の魔女 <megablacklabel@gmail.com>
2024-12-23 02:57:02 +0000

    feat: Implement solution for the Populating Next Right Pointers in Each Node problem

---
+++ 116.populating-next-right-pointers-in-each-node.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode id=116 lang=python3
+#
+# [116] Populating Next Right Pointers in Each Node
+#
+
+# @lc code=start
+
+from typing import Optional
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    def connect(self, root: Optional[Node]) -> Optional[Node]:
+        if not root:
+            return None
+
+        def connectTwoNodes(p, q) -> None:
+            if not p:
+                return
+            p.next = q
+            connectTwoNodes(p.left, p.right)
+            connectTwoNodes(q.left, q.right)
+            connectTwoNodes(p.right, q.left)
+
+        connectTwoNodes(root.left, root.right)
+        return root
+# @lc code=end
+
