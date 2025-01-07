d07ddb4019a17eba6997cf8e192a1376caaaf9dc
底なし沼の魔女 <megablacklabel@gmail.com>
2025-01-07 03:34:48 +0000

    feat: Implement solution for the Reorder List problem

---
+++ 143.reorder-list.py
@@ -0,0 +1,61 @@
+#
+# @lc app=leetcode id=143 lang=python3
+#
+# [143] Reorder List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        def findMid(head: ListNode):
+            prev = None
+            slow = head
+            fast = head
+
+            while fast and fast.next:
+                prev = slow
+                slow = slow.next
+                fast = fast.next.next
+            prev.next = None
+
+            return slow
+
+        def reverse(head: ListNode) -> ListNode:
+            prev = None
+            curr = head
+
+            while curr:
+                next = curr.next
+                curr.next = prev
+                prev = curr
+                curr = next
+
+            return prev
+
+        def merge(l1: ListNode, l2: ListNode) -> None:
+            while l2:
+                next = l1.next
+                l1.next = l2
+                l1 = l2
+                l2 = next
+
+        if not head or not head.next:
+            return
+
+        mid = findMid(head)
+        reversed = reverse(mid)
+        merge(head, reversed)
+        
+# @lc code=end
+
