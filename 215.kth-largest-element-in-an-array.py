c4a745d96e3416827abcf4bb0a8d4d9cf3cbcb49
底なし沼の魔女 <megablacklabel@gmail.com>
2025-02-06 03:05:17 +0000

    feat: Implement solution for Kth Largest Element in an Array

---
+++ 215.kth-largest-element-in-an-array.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=215 lang=python3
+#
+# [215] Kth Largest Element in an Array
+#
+
+# @lc code=start
+import heapq
+from typing import List
+
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        minHeap = []
+
+        for num in nums:
+            heapq.heappush(minHeap, num)
+            if len(minHeap) > k:
+                heapq.heappop(minHeap)
+
+        return minHeap[0]
+# @lc code=end
+
