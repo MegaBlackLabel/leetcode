1aaa2d3ec5f8c39b7326ae5500cfc1199479af07
底なし沼の魔女 <megablacklabel@gmail.com>
2025-03-12 03:30:29 +0000

    feat: Implement solution for Odd Even Linked List problem

---
+++ 328.odd-even-linked-list.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode id=328 lang=python3
+#
+# [328] Odd Even Linked List
+#
+
+# @lc code=start
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+
+        odd = head
+        even = head.next
+        even_head = even
+
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+
+        odd.next = even_head
+
+        return head
+
+# @lc code=end
+
