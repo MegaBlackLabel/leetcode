e3501d550b1b4c0540bdbe0f32b7869cdd4d8c1e
底なし沼の魔女 <megablacklabel@gmail.com>
2025-03-14 03:30:15 +0000

    feat: Implement solution for Increasing Triplet Subsequence problem

---
+++ 334.increasing-triplet-subsequence.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=334 lang=python3
+#
+# [334] Increasing Triplet Subsequence
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        first = second = float('inf')
+
+        for num in nums:
+            if num <= first:
+                first = num
+            elif num <= second:
+                second = num
+            else:
+                return True
+
+        return False
+        
+# @lc code=end
+
