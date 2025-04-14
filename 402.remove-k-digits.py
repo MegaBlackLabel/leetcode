9dc7296900847a6338d2f155a56bb4917784867f
底なし沼の魔女 <megablacklabel@gmail.com>
2025-04-14 02:41:44 +0000

    feat: Implement solution for Remove K Digits problem

---
+++ 402.remove-k-digits.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=402 lang=python3
+#
+# [402] Remove K Digits
+#
+
+# @lc code=start
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        if len(num) == k:
+            return '0'
+
+        ans = []
+        stack = []
+
+        for i, digit in enumerate(num):
+            while k > 0 and stack and stack[-1] > digit:
+                stack.pop()
+                k -= 1
+            stack.append(digit)
+
+        for _ in range(k):
+            stack.pop()
+
+        for c in stack:
+            if c == '0' and not ans:
+                continue
+            ans.append(c)
+
+        return ''.join(ans) if ans else '0'
+# @lc code=end
+
