8580b989956116c1ff43f72a82f0555842a270ba
底なし沼の魔女 <megablacklabel@gmail.com>
2025-06-20 03:41:35 +0000

    feat: Implement solution for Single Element in a Sorted Array problem

---
+++ 540.single-element-in-a-sorted-array.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=540 lang=python3
+#
+# [540] Single Element in a Sorted Array
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        
+        left, right = 0, len(nums) - 1
+        
+        while left < right:
+            mid = (left + right) // 2
+            
+            if mid % 2 == 1:
+                mid -= 1
+            
+            if nums[mid] == nums[mid + 1]:
+                left = mid + 2
+            else:
+                right = mid
+        
+        return nums[left]
+# @lc code=end
+
