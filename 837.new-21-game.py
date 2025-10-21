afe35ecd4ac482bab55d903b02a2a9b72b85bd53
底なし沼の魔女 <megablacklabel@gmail.com>
2025-10-21 03:20:54 +0000

    feat: Add solution for New 21 Game problem

---
+++ 837.new-21-game.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=837 lang=python3
+#
+# [837] New 21 Game
+#
+
+# @lc code=start
+class Solution:
+    def new21Game(self, n: int, k: int, maxPts: int) -> float:
+        
+        if k == 0 or n >= k + maxPts:
+            return 1.0
+
+        dp = [0.0] * (n + 1)
+        dp[0] = 1.0
+        window_sum = 1.0
+        result = 0.0
+
+        for i in range(1, n + 1):
+            dp[i] = window_sum / maxPts
+            if i < k:
+                window_sum += dp[i]
+            else:
+                result += dp[i]
+            if i - maxPts >= 0:
+                window_sum -= dp[i - maxPts]
+
+        return result
+# @lc code=end
+
