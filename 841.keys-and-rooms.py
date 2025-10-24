7d07ecdbd01a58b17021b79df19fa8235dbc2ae1
底なし沼の魔女 <megablacklabel@gmail.com>
2025-10-24 03:04:34 +0000

    feat: Implement solution for Keys and Rooms problem

---
+++ 841.keys-and-rooms.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=841 lang=python3
+#
+# [841] Keys and Rooms
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        
+        visited = set()
+
+        def dfs(room: int) -> None:
+            visited.add(room)
+            for key in rooms[room]:
+                if key not in visited:
+                    dfs(key)
+
+        dfs(0)
+        return len(visited) == len(rooms)
+# @lc code=end
+
