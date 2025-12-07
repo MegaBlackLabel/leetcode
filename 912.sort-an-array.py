010552a679f067d1b8838493aa597b9294a6d2ca
底なし沼の魔女 <megablacklabel@gmail.com>
2025-12-07 02:48:20 +0000

    feat: Add solution for Sort an Array problem

---
+++ 912.sort-an-array.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode id=912 lang=python3
+#
+# [912] Sort an Array
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        
+        return sorted(nums)
+# @lc code=end
+
