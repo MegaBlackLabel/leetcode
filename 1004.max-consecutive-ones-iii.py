2436691b3b18a13c3f6bb2f13b6b5f22a74da010
底なし沼の魔女 <megablacklabel@gmail.com>
2026-02-01 11:52:37 +0000

    feat: Add solution for Max Consecutive Ones III problem

---
+++ 1004.max-consecutive-ones-iii.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=1004 lang=python3
+#
+# [1004] Max Consecutive Ones III
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        
+        left = 0
+        zeros_count = 0
+        max_length = 0
+
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                zeros_count += 1
+
+            while zeros_count > k:
+                if nums[left] == 0:
+                    zeros_count -= 1
+                left += 1
+
+            max_length = max(max_length, right - left + 1)
+
+        return max_length
+# @lc code=end
+
