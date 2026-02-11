5a15b126031513dd902174dc937505edac0265f1
底なし沼の魔女 <megablacklabel@gmail.com>
2026-02-11 04:18:42 +0000

    add

---
+++ 1015.smallest-integer-divisible-by-k.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=1015 lang=python3
+#
+# [1015] Smallest Integer Divisible by K
+#
+
+# @lc code=start
+class Solution:
+    def smallestRepunitDivByK(self, k: int) -> int:
+        
+        if k % 10 not in {1, 3, 7, 9}:
+            return -1
+
+        seen = set()
+        n = 0
+
+        for length in range(1, k + 1):
+            n = (n * 10 + 1) % k
+            if n == 0:
+                return length
+            if n in seen:
+                return -1
+            seen.add(n)
+
+        return -1
+# @lc code=end
+
