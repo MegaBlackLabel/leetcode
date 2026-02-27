9ef94b7005efb04a41d0d3cf06025e9510322654
底なし沼の魔女 <megablacklabel@gmail.com>
2026-02-27 02:48:31 +0000

    feat: Implement solution for Distribute Elements Into Two Arrays I problem

---
+++ 3069.distribute-elements-into-two-arrays-i.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=3069 lang=python3
+#
+# [3069] Distribute Elements Into Two Arrays I
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def resultArray(self, nums: List[int]) -> List[int]:
+        
+        arr1 = [nums[0]]
+        arr2 = [nums[1]]
+        
+        for i in range(2, len(nums)):
+            if arr1[-1] > arr2[-1]:
+               
+                arr1.append(nums[i])
+            else:
+                arr2.append(nums[i])
+                
+        return arr1 + arr2
+# @lc code=end
+
