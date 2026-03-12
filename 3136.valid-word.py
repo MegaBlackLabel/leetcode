a58a0cfc85a051f060ee29a64d72766733a0a999
底なし沼の魔女 <megablacklabel@gmail.com>
2026-03-12 03:26:29 +0000

    feat: Add solution for Valid Word problem

---
+++ 3136.valid-word.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=3136 lang=python3
+#
+# [3136] Valid Word
+#
+
+# @lc code=start
+class Solution:
+    def isValid(self, word: str) -> bool:
+        if not word.isalnum() :
+            return False
+
+        if len(word) < 3 :
+            return False
+
+        cons, vow = False, False
+        vowels = set('aeiou')
+
+        for i, c in enumerate(word) :
+            if c.lower() in vowels :
+                vow = True
+            elif not c.isnumeric() :
+                cons = True
+            
+            if vow and cons :
+                return True
+        return False
+# @lc code=end
+
