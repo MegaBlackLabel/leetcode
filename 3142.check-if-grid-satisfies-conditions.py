d505a0550cce79651b4b4a7777ecacf9746571f3
底なし沼の魔女 <megablacklabel@gmail.com>
2026-03-13 03:58:56 +0000

    feat: Implement solution for Check if Grid Satisfies Conditions problem

---
+++ 3142.check-if-grid-satisfies-conditions.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=3142 lang=python3
+#
+# [3142] Check if Grid Satisfies Conditions
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def satisfiesConditions(self, grid: List[List[int]]) -> bool:
+        
+        m = len(grid)
+        n = len(grid[0])
+        
+        for i in range(m):
+            for j in range(n):
+                if i + 1 < m:
+                    if grid[i][j] != grid[i + 1][j]:
+                        return False
+                
+                if j + 1 < n:
+                    if grid[i][j] == grid[i][j + 1]:
+                        return False
+                        
+        return True
+# @lc code=end
+
