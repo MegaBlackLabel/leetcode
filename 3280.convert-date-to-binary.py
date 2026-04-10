e3469320ad8c41edb47a168dbfc2c761e679f820
底なし沼の魔女 <megablacklabel@gmail.com>
2026-04-10 03:34:40 +0000

    feat: Add solution for Convert Date to Binary problem

---
+++ 3280.convert-date-to-binary.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=3280 lang=python3
+#
+# [3280] Convert Date to Binary
+#
+
+# @lc code=start
+class Solution:
+    def convertDateToBinary(self, date: str) -> str:
+         return "-".join(bin(int(s))[2:] for s in date.split("-"))
+# @lc code=end
+
