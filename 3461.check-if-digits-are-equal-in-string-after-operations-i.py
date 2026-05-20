53438b2c9af3794838093e8633b435c8871179a8
底なし沼の魔女 <megablacklabel@gmail.com>
2026-05-20 03:48:55 +0000

    feat: Add solution for Check If Digits Are Equal in String After Operations I problem

---
+++ 3461.check-if-digits-are-equal-in-string-after-operations-i.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=3461 lang=python3
+#
+# [3461] Check If Digits Are Equal in String After Operations I
+#
+
+# @lc code=start
+class Solution:
+    def hasSameDigits(self, s: str) -> bool:
+        while len(s) > 2:
+            new_s = []
+            for i in range(len(s) - 1):
+                digit_sum = (int(s[i]) + int(s[i+1])) % 10
+                new_s.append(str(digit_sum))
+            s = "".join(new_s)
+        
+        return s[0] == s[1]
+# @lc code=end
+
