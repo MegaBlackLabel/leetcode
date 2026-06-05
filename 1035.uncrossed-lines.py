0af18e04a29fb42796aecbcfa5ad11a861f5d231
底なし沼の魔女 <megablacklabel@gmail.com>
2026-06-05 03:27:08 +0000

    feat: Add solution for Uncrossed Lines problem

---
+++ 1035.uncrossed-lines.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=1035 lang=python3
+#
+# [1035] Uncrossed Lines
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
+        m, n = len(nums1), len(nums2)
+        
+        if m < n:
+            nums1, nums2 = nums2, nums1
+            m, n = n, m
+            
+        dp = [0] * (n + 1)
+        
+        for i in range(1, m + 1):
+            prev = 0
+            for j in range(1, n + 1):
+                temp = dp[j]
+                if nums1[i - 1] == nums2[j - 1]:
+                    dp[j] = prev + 1
+                else:
+                    dp[j] = max(dp[j], dp[j - 1])
+                prev = temp
+                
+        return dp[n]
+# @lc code=end
+
