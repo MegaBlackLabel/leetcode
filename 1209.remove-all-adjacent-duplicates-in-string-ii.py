aa2629b8741e884f1746b70f15732ba6431ed760
底なし沼の魔女 <megablacklabel@gmail.com>
2026-08-31 03:47:54 +0000

    feat: Implement solution for Remove All Adjacent Duplicates in String II problem

---
+++ 1209.remove-all-adjacent-duplicates-in-string-ii.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=1209 lang=python3
+#
+# [1209] Remove All Adjacent Duplicates in String II
+#
+
+# @lc code=start
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        stack = [] 
+        
+        for char in s:
+            if stack and stack[-1][0] == char:
+                stack[-1][1] += 1
+                if stack[-1][1] == k:
+                    stack.pop()
+            else:
+                stack.append([char, 1])
+                
+        return "".join(char * count for char, count in stack)
+# @lc code=end
+
