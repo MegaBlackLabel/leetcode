d85908fdcf6ecb6015c6fe8e823cf934472759cb
底なし沼の魔女 <megablacklabel@gmail.com>
2026-09-25 03:45:01 +0000

    feat: Implement solution for Greatest Sum Divisible by Three problem

---
+++ 1262.greatest-sum-divisible-by-three.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=1262 lang=python3
+#
+# [1262] Greatest Sum Divisible by Three
+#
+
+# @lc code=start
+class Solution:
+    def maxSumDivThree(self, nums: list[int]) -> int:
+        n = len(nums)
+        dp = [[-float('inf')] * 3 for _ in range(n + 1)]
+        dp[0][0] = 0
+
+        for i in range(1, n + 1):
+            current_num = nums[i - 1]
+          
+            for remainder in range(3):
+                dp[i][remainder] = dp[i - 1][remainder]
+                prev_remainder = (remainder - current_num) % 3
+                dp[i][remainder] = max(
+                    dp[i][remainder], 
+                    dp[i - 1][prev_remainder] + current_num
+                )
+      
+        return dp[n][0]
+# @lc code=end
+
